b4512e71afd3b434e84b9f55e75cb0f0e68b3fb7
cf <aichenfuxi@qq.com>
2018-05-27 02:41:38 +0800

    优化界面

--- js/play.js
+++ js/play.js
@@ -16,8 +16,28 @@ $.get('lyric.json').then(function(object) {
 		$p.attr('data-time', object.time).text(object.words)
 		$p.appendTo($lyric)
 		})
-	})
+})
+	let audio=document.createElement('audio')
+	//audio.autoplay='autoplay'  自动播放  手机无效
 
+	audio.src='http://dl.stream.qqmusic.qq.com/C400000VOBAv1zQol0.m4a?guid=3712541126&vkey=899DB7123C76ED8A38DD4E6ACF02077FA1C399D94CB14BCFAC727396E263E7C4E94D61CEFBC50151AE313976ED9A6E237B4654A183E09529&uin=0&fromtag=38'
+	
+	//  进入页面自动播放 手机无效
+	// audio.oncanplay=function(){
+       // audio.play()
+        //$('.disc').addClass('playing')
+		//}
+		$('.btn-stop').on('click',function(){
+			audio.pause()
+			$('.musiccontrol').removeClass('playing')
+			$('.disc').removeClass('playing')
+			
+		})
+		$('.btn-play').on('click',function(){
+			audio.play()
+			$('.musiccontrol').addClass('playing')
+			$('.disc').addClass('playing')
+		})
 })
 
 $(contaiter).on('click',function(){
